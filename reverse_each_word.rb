d3e14ecb78182608651a07d95cf670e66d0be8e0
Vianka Rivera <viankalrivera@gmail.com>
2020-05-07 23:57:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,12 @@
 def reverse_each_word(sentence1)
   sentence1.reverse
+  #original_array = string.split(" ")
+  #return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
 end
 
 def reverse_each_word(sentence2)
